125758dad91d646e6718d655c3241014d255eb24
Mert <mmertartar+github@gmail.com>
2022-10-30 17:30:40 +0300

    day-0-hello-world solved

---
+++ 30-days-of-code/day-0-hello-world.js
@@ -0,0 +1,37 @@
+/*
+task name = 
+Day 0: Hello, World.
+
+taks =
+To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally print the value of your variable on a second line.
+You've got this!
+
+input = 
+Welcome to 30 Days of Code!
+
+expected output =
+Hello, World. 
+Welcome to 30 Days of Code!
+*/
+
+//solution of the problem with explanation =
+function processData(inputString) {
+  // Javascript dilinde console ekranina birsey yazdirmak icin console.log(parameter) kullaniriz
+  console.log("Hello, World.");
+  // Bize verilen degiskeni console.log(parameter) kullanarak console ekranina yazdirdik
+  console.log(inputString);
+}
+
+//solution of the problem(code) =
+function processData(inputString) {
+  console.log("Hello, World.");
+  console.log(inputString);
+}
+
+/*
+
+output = 
+>>Hello, World. 
+>>Welcome to 30 Days of Code!
+
+*/
